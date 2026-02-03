efad0d7ed464fb163b89077f0b9dd3458ca1b61a
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-02-03 23:13:46 +1100

    fix typos

--- shared/src/services/transformWord.ts
+++ shared/src/services/transformWord.ts
@@ -2,7 +2,7 @@ import { DictionaryResponse,Word } from "../types";
 
 export function transformToWord(
     apiResponse:DictionaryResponse,
-    useId:string,
+    userId:string,
     sourceUrl?:string,
 ):Omit<Word, 'id'|'createdAt'|'updatedAt'> {
     return {
@@ -10,7 +10,7 @@ export function transformToWord(
         definition: apiResponse.meanings[0]?.definitions[0]?.definition || "Definition not found.",
         partOfSpeech: apiResponse.meanings[0]?.partOfSpeech || "N/A",
         example: apiResponse.meanings[0]?.definitions[0]?.example,
-        userId: useId,
+        userId: userId,
         phonetics: apiResponse.phonetics.find(p => p.text)?.text,
         sourceUrl: sourceUrl,
     };
